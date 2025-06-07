bd4763f2b1abec39a34914793bbdec6e895e5cef
Sören Tempel <soeren+git@soeren-tempel.net>
2025-06-07 07:23:52 +0200

    quebex: Add custom state module

--- quebex/src/Language/QBE/Simulator.hs
+++ quebex/src/Language/QBE/Simulator.hs
@@ -9,68 +9,14 @@ module Language.QBE.Simulator
   )
 where
 
-import Control.Monad.Except (ExceptT, liftEither, runExceptT, throwError)
+import Control.Monad.Except (liftEither, runExceptT)
 import Control.Monad.IO.Class (liftIO)
-import Control.Monad.State (StateT, get, put, runStateT)
+import Control.Monad.State (get, put, runStateT)
 import Data.Functor ((<&>))
-import Data.Map qualified as Map
 import Language.QBE.Simulator.Error
 import Language.QBE.Simulator.Expression
-import Language.QBE.Simulator.Memory
+import Language.QBE.Simulator.State
 import Language.QBE.Types qualified as QBE
-
--- TODO: Turns this into a Monad or use lenses or something?
-data Env
-  = Env
-  { envVars :: Map.Map String RegVal,
-    envMem :: Memory,
-    envStkPtr :: Address
-  }
-
-mkEnv :: Address -> Size -> IO Env
-mkEnv a s = do
-  mem <- mkMemory a s
-  return $ Env Map.empty mem (s - 1)
-
-pushStack :: Size -> Address -> Exec RegVal
-pushStack size align = do
-  (Env vars mem stkPtr) <- get
-  let newStkPtr = alignAddr (stkPtr - size) align
-  put (Env vars mem newStkPtr)
-  return $ ELong newStkPtr
-  where
-    alignAddr :: Address -> Address -> Address
-    alignAddr addr alignment = addr - (addr `mod` alignment)
-
-insertValue :: String -> RegVal -> Exec Env
-insertValue k v = do
-  (Env vars mem ptr) <- get
-  return $ Env (Map.insert k v vars) mem ptr
-
-lookupValue' :: String -> Exec RegVal
-lookupValue' k = do
-  e <- get
-  case Map.lookup k (envVars e) of
-    Nothing -> throwError UnknownVariable
-    Just v -> pure v
-
-lookupValue :: QBE.BaseType -> QBE.Value -> Exec RegVal
-lookupValue ty (QBE.VConst (QBE.Const (QBE.Number v))) =
-  pure $ case ty of
-    QBE.Long -> ELong v
-    QBE.Word -> EWord $ fromIntegral v
-    QBE.Single -> ESingle $ fromIntegral v
-    QBE.Double -> EDouble $ fromIntegral v
-lookupValue _ (QBE.VConst (QBE.Const (QBE.SFP v))) = pure $ ESingle v
-lookupValue _ (QBE.VConst (QBE.Const (QBE.DFP v))) = pure $ EDouble v
-lookupValue _ (QBE.VConst (QBE.Const (QBE.Global k))) =
-  lookupValue' (show k)
-lookupValue _ (QBE.VConst (QBE.Thread k)) =
-  lookupValue' (show k)
-lookupValue _ (QBE.VLocal k) =
-  lookupValue' (show k)
-
-type Exec a = StateT Env (ExceptT EvalError IO) a
 
 execVolatile :: QBE.VolatileInstr -> Exec Env
 execVolatile = error "execVolatile not implemented"
---
+++ quebex/src/Language/QBE/Simulator/State.hs
@@ -0,0 +1,62 @@
+module Language.QBE.Simulator.State where
+
+import Control.Monad.Except (ExceptT, throwError)
+import Control.Monad.State (StateT, get, put)
+import Data.Map qualified as Map
+import Language.QBE.Simulator.Error
+import Language.QBE.Simulator.Expression
+import Language.QBE.Simulator.Memory
+import Language.QBE.Types qualified as QBE
+
+type Exec a = StateT Env (ExceptT EvalError IO) a
+
+-- TODO: Turns this into a Monad or use lenses or something?
+data Env
+  = Env
+  { envVars :: Map.Map String RegVal,
+    envMem :: Memory,
+    envStkPtr :: Address
+  }
+
+mkEnv :: Address -> Size -> IO Env
+mkEnv a s = do
+  mem <- mkMemory a s
+  return $ Env Map.empty mem (s - 1)
+
+pushStack :: Size -> Address -> Exec RegVal
+pushStack size align = do
+  (Env vars mem stkPtr) <- get
+  let newStkPtr = alignAddr (stkPtr - size) align
+  put (Env vars mem newStkPtr)
+  return $ ELong newStkPtr
+  where
+    alignAddr :: Address -> Address -> Address
+    alignAddr addr alignment = addr - (addr `mod` alignment)
+
+insertValue :: String -> RegVal -> Exec Env
+insertValue k v = do
+  (Env vars mem ptr) <- get
+  return $ Env (Map.insert k v vars) mem ptr
+
+lookupValue' :: String -> Exec RegVal
+lookupValue' k = do
+  e <- get
+  case Map.lookup k (envVars e) of
+    Nothing -> throwError UnknownVariable
+    Just v -> pure v
+
+lookupValue :: QBE.BaseType -> QBE.Value -> Exec RegVal
+lookupValue ty (QBE.VConst (QBE.Const (QBE.Number v))) =
+  pure $ case ty of
+    QBE.Long -> ELong v
+    QBE.Word -> EWord $ fromIntegral v
+    QBE.Single -> ESingle $ fromIntegral v
+    QBE.Double -> EDouble $ fromIntegral v
+lookupValue _ (QBE.VConst (QBE.Const (QBE.SFP v))) = pure $ ESingle v
+lookupValue _ (QBE.VConst (QBE.Const (QBE.DFP v))) = pure $ EDouble v
+lookupValue _ (QBE.VConst (QBE.Const (QBE.Global k))) =
+  lookupValue' (show k)
+lookupValue _ (QBE.VConst (QBE.Thread k)) =
+  lookupValue' (show k)
+lookupValue _ (QBE.VLocal k) =
+  lookupValue' (show k)
