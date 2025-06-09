ede0b256ff72ebf93a168d5301be103e0d089e8e
Sören Tempel <soeren+git@soeren-tempel.net>
2025-06-09 08:02:16 +0200

    Slightly refactor Simulator tests

--- quebex/test/Simulator.hs
+++ quebex/test/Simulator.hs
@@ -1,7 +1,9 @@
 module Simulator (simTests) where
 
+import Control.Monad.State (get)
 import Data.ByteString.Lazy qualified as BSL
 import Data.Map qualified as Map
+import Language.QBE.Parser (funcDef)
 import Language.QBE.Simulator
 import Language.QBE.Simulator.Expression qualified as E
 import Language.QBE.Simulator.Memory qualified as MEM
@@ -9,83 +11,77 @@ import Language.QBE.Simulator.State
 import Language.QBE.Types
 import Test.Tasty
 import Test.Tasty.HUnit
+import Text.ParserCombinators.Parsec qualified as P
+
+parseAndExec :: String -> IO BlockResult
+parseAndExec func = do
+  fDef <- case P.parse funcDef "execFunc" func of
+    Left e -> fail $ "Unexpected parsing error: " ++ show e
+    Right r -> pure r
+
+  evalRes <- runExec (execFunc fDef)
+  case evalRes of
+    Left e -> fail $ "Unexpected evaluation error: " ++ show e
+    Right r -> pure r
+
+------------------------------------------------------------------------
 
 blockTests :: TestTree
 blockTests =
   testGroup
     "Evaluation of Basic Blocks"
-    [ testCase "Evaluate single basic block" $
+    [ testCase "Evaluate single basic block with single instruction" $
         do
-          let i =
-                Assign
-                  (LocalIdent "val")
-                  Word
-                  ( Add
-                      (VConst (Const (Number 1)))
-                      (VConst (Const (Number 2)))
-                  )
-          let b = Block {label = BlockIdent "start", stmt = [i], term = Return Nothing}
+          res <-
+            parseAndExec
+              "function w $addNumbers() {\n\
+              \@start\n\
+              \%c =w add 1, 2\n\
+              \ret %c\n\
+              \}\n"
 
-          res <- runExec (execFunc $ makeFunc [b])
-          assertEqual "" (envVars <$> res) $ Right (Map.fromList [(LocalIdent "val", E.VWord 3)]),
-      testCase "Evaluate multiple basic blocks" $
+          res @?= Left (Just $ E.VWord 3),
+      testCase "Evaluate single basic block with multiple instructions" $
         do
-          let i1 =
-                Assign
-                  (LocalIdent "val")
-                  Word
-                  ( Add
-                      (VConst (Const (Number 1)))
-                      (VConst (Const (Number 2)))
-                  )
-          let i2 =
-                Assign
-                  (LocalIdent "foo")
-                  Word
-                  ( Add
-                      (VLocal (LocalIdent "val"))
-                      (VConst (Const (Number 2)))
-                  )
-          let b = Block {label = BlockIdent "calc", stmt = [i1, i2], term = Return Nothing}
+          res <-
+            parseAndExec
+              "function w $addMultiple() {\n\
+              \@begin\n\
+              \%val =w add 1, 2\n\
+              \%foo =w add %val, 2\n\
+              \ret %foo\n\
+              \}\n"
 
-          res <- runExec (execFunc $ makeFunc [b])
-          assertEqual "" (envVars <$> res) $ Right (Map.fromList [(LocalIdent "val", E.VWord 3), (LocalIdent "foo", E.VWord 5)]),
+          res @?= Left (Just $ E.VWord 5),
       testCase "Evaluate expression with subtyping" $
         do
-          let i1 =
-                Assign
-                  (LocalIdent "val")
-                  Long
-                  ( Add
-                      (VConst (Const (Number 0xdeadbeefdecafbad)))
-                      (VConst (Const (Number 0)))
-                  )
-          let i2 =
-                Assign
-                  (LocalIdent "foo")
-                  Word
-                  ( Add
-                      (VLocal (LocalIdent "val"))
-                      (VConst (Const (Number 0)))
-                  )
-          let b = Block {label = BlockIdent "subtype", stmt = [i1, i2], term = Return Nothing}
+          res <-
+            -- 16045690984835251117 == 0xdeadbeefdecafbad
+            parseAndExec
+              "function w $subtyping() {\n\
+              \@go\n\
+              \%val =l add 16045690984835251117, 0\n\
+              \%foo =w add %val, 0\n\
+              \ret %foo\n\
+              \}\n"
 
-          res <- runExec (execFunc $ makeFunc [b])
-          assertEqual "" (envVars <$> res) $ Right (Map.fromList [(LocalIdent "val", E.VLong 0xdeadbeefdecafbad), (LocalIdent "foo", E.VWord 0xdecafbad)]),
+          res @?= Left (Just $ E.VWord 0xdecafbad),
+      -- TODO: Rewrite once we have load instruction support.
       testCase "Alloc pre-aligned value on stack" $
         do
           let i = Assign (LocalIdent "ptr") Long (Alloc AlignWord 4)
           let b = Block {label = BlockIdent "allocate", stmt = [i], term = Return Nothing}
 
-          res <- runExec (execFunc $ makeFunc [b])
+          res <- runExec (execFunc (makeFunc [b]) >> get)
           assertEqual "" (envVars <$> res) $ Right (Map.fromList [(LocalIdent "ptr", E.VLong 120)]),
+      -- TODO: Rewrite once we have load instruction support.
       testCase "Store value on the stack" $
         do
           let i1 = Assign (LocalIdent "ptr") Long (Alloc AlignWord 4)
           let i2 = Volatile $ Store (Base Word) (VConst (Const (Number 0x42))) (VLocal (LocalIdent "ptr"))
           let bl = Block {label = BlockIdent "allocate", stmt = [i1, i2], term = Return Nothing}
 
-          env <- runExec (execFunc $ makeFunc [bl])
+          env <- runExec (execFunc (makeFunc [bl]) >> get)
           let (Right mem) = envMem <$> env
 
           res <- MEM.loadByteString mem 120 4
