0dd63d1359238211fc3bb76e9a6036664ec9a5f4
Sören Tempel <soeren+git@soeren-tempel.net>
2025-06-21 15:30:12 +0200

    quebex-symex: Start working on Simulator tests

--- quebex-symex/test/Main.hs
+++ quebex-symex/test/Main.hs
@@ -1,11 +1,15 @@
 module Main (main) where
 
-import Concolic qualified as C
-import Symbolic qualified as S
+import Concolic qualified as CE
+import Simulator qualified as SIM
+import Symbolic qualified as SE
 import Test.Tasty
 
 main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Tests" [S.exprTests, C.exprTests]
+tests =
+  testGroup
+    "Tests"
+    [SE.exprTests, CE.exprTests, SIM.simTests]
---
+++ quebex-symex/test/Simulator.hs
@@ -0,0 +1,60 @@
+module Simulator (simTests) where
+
+import Control.Monad.State (gets)
+import Data.List (find)
+import Language.QBE (globalFuncs, parse)
+import Language.QBE.Simulator
+import Language.QBE.Simulator.Concolic.Expression qualified as CE
+import Language.QBE.Simulator.State (envTracer)
+import Language.QBE.Simulator.Symbolic.Tracer qualified as ST
+import Language.QBE.Types qualified as QBE
+import Test.Tasty
+import Test.Tasty.HUnit
+
+-- TODO: Code duplication with quebex/test/Simulator.hs
+parseAndExec :: QBE.GlobalIdent -> [CE.Concolic] -> String -> IO (ST.ExecTrace)
+parseAndExec funcName params input = do
+  prog <- case parse "" input of
+    Left e -> fail $ "Unexpected parsing error: " ++ show e
+    Right r -> pure r
+
+  func <- case find (\f -> QBE.fName f == funcName) (globalFuncs prog) of
+    Just x -> pure x
+    Nothing -> fail $ "Unknown function: " ++ show funcName
+
+  sTrace <- runExec prog (execFunc func params >> gets envTracer) ST.newExecTrace
+  case sTrace of
+    Left e -> fail $ "Unexpected evaluation error: " ++ show e
+    Right r -> pure r
+
+------------------------------------------------------------------------
+
+traceTests :: TestTree
+traceTests =
+  testGroup
+    "Tests for the Symbolic Tracer"
+    [ testCase "Branch tracing with single concrete branch" $
+        do
+          t <-
+            parseAndExec
+              (QBE.GlobalIdent "main")
+              []
+              "function $main() {\n\
+              \@start.1\n\
+              \%cond =w add 0, 1\n\
+              \jnz %cond, @branch.1, @branch.2\n\
+              \@branch.1\n\
+              \ret\n\
+              \@branch.2\n\
+              \ret\n\
+              \}"
+
+          -- Trace must be empty because it doesn't branch on symbolic values.
+          length t @?= 0
+    ]
+
+simTests :: TestTree
+simTests =
+  testGroup
+    "Tests for the Symbolic Simulator"
+    [traceTests]
