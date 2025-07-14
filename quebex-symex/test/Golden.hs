2aa9a4a8a2c7f7906d8734a38f3b5f2ee36308a6
Sören Tempel <soeren+git@soeren-tempel.net>
2025-07-14 14:07:28 +0200

    quebex-symex: Start working on golden tests

---
+++ quebex-symex/test/Golden.hs
@@ -0,0 +1,63 @@
+-- SPDX-FileCopyrightText: 2025 Sören Tempel <soeren+git@soeren-tempel.net>
+--
+-- SPDX-License-Identifier: GPL-3.0-only
+
+module Golden (goldenTests) where
+
+import Data.List (find)
+import Language.QBE (globalFuncs, parse)
+import Language.QBE.Simulator.Explorer (explore)
+import Language.QBE.Types qualified as QBE
+import System.FilePath
+import Test.Tasty
+import Test.Tasty.Golden.Advanced
+
+type Result = Int
+
+entryFunc :: QBE.GlobalIdent
+entryFunc = QBE.GlobalIdent "entry"
+
+exploreQBE :: FilePath -> [(String, QBE.BaseType)] -> IO Result
+exploreQBE filePath params = do
+  content <- readFile filePath
+  prog <- case parse filePath content of
+    Right rt -> pure rt
+    Left err -> fail $ "Parsing error: " ++ show err
+
+  let funcs = globalFuncs prog
+  func <- case find (\f -> QBE.fName f == entryFunc) funcs of
+    Just x -> pure x
+    Nothing -> fail $ "Unable to find entry function: " ++ show entryFunc
+
+  traces <- explore prog func params
+  pure $ length traces
+
+simpleCmp :: Result -> Result -> IO (Maybe String)
+simpleCmp expt act =
+  return $
+    if expt == act
+      then Nothing
+      else Just ("Exploration mismatch: " ++ err)
+  where
+    err :: String
+    err = "expected=" ++ show expt ++ " actual=" ++ show act
+
+runTest :: TestName -> Int -> [(String, QBE.BaseType)] -> TestTree
+runTest testName expPaths params =
+  goldenTest
+    testName
+    (pure expPaths)
+    (exploreQBE fullPath params)
+    simpleCmp
+    (\_ -> pure ())
+  where
+    fullPath :: FilePath
+    fullPath = "test" </> "golden" </> (testName ++ ".qbe")
+
+------------------------------------------------------------------------
+
+goldenTests :: TestTree
+goldenTests =
+  testGroup
+    "goldenTests"
+    [runTest "three-branches" 3 [("a", QBE.Word), ("b", QBE.Word)]]
