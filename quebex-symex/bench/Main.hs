9f5f12fc422b33dde93a0b6423044f575656c09b
Sören Tempel <soeren+git@soeren-tempel.net>
2025-08-06 02:58:59 +0200

    quebex-symex: print benchmark results

--- quebex-symex/bench/Main.hs
+++ quebex-symex/bench/Main.hs
@@ -1,18 +1,20 @@
 module Main (main) where
 
-import System.IO (withFile, IOMode(WriteMode))
-import System.Process (shell, createProcess, waitForProcess)
-import System.Exit (ExitCode(ExitSuccess))
-import System.Directory (listDirectory)
-import System.FilePath ((</>))
-import Control.Monad (forM_)
-import Data.List (find)
+import Control.Monad (forM_, when)
+import Data.Char (isLower)
+import Data.List (find, unsnoc)
 import Language.QBE (globalFuncs, parse)
-import Language.QBE.Simulator.Explorer (explore, logSolver, newEngine)
-import Language.QBE.Types qualified as QBE
+import Language.QBE.Backend.QuerySplitter (splitQueries)
 import Language.QBE.Backend.Store qualified as ST
 import Language.QBE.Backend.Tracer qualified as T
-import Language.QBE.Backend.QuerySplitter (splitQueries)
+import Language.QBE.Simulator.Explorer (explore, logSolver, newEngine)
+import Language.QBE.Types qualified as QBE
+import SimpleSMT qualified as SMT
+import System.Directory (listDirectory)
+import System.Exit (ExitCode (ExitSuccess))
+import System.FilePath (takeBaseName, (</>))
+import System.IO (IOMode (WriteMode), hGetContents, withFile)
+import System.Process (StdStream (CreatePipe), createProcess, shell, std_out, waitForProcess)
 
 logPath :: FilePath
 logPath = "/tmp/quebex-symex-bench.smt2"
@@ -33,33 +35,51 @@ exploreQBE filePath params = do
     Nothing -> fail $ "Unable to find entry function: " ++ show entryFunc
 
   withFile logPath WriteMode (explore' prog func)
- where
-  explore' prog func handle = do
-    engine <- logSolver handle >>= newEngine
-    explore engine prog func params
+  where
+    explore' prog func handle = do
+      engine <- logSolver handle >>= newEngine
+      explore engine prog func params
 
 getQueries :: FilePath -> FilePath -> [(String, QBE.BaseType)] -> IO ()
 getQueries queryFp qbeFp params = do
   _ <- exploreQBE qbeFp params
   splitQueries logPath queryFp
 
-solveQueries :: FilePath -> IO ()
+solveQueries :: FilePath -> IO [(FilePath, Double)]
 solveQueries dirFp = do
   queries <- map (dirFp </>) <$> listDirectory dirFp
-  forM_ queries solveQuery
- where
-  solveQuery :: FilePath -> IO ()
-  solveQuery fp = do
-    -- TODO: use createProcess and do the input redirection in Haskell.
-    (_, _, _, p) <-
-       createProcess $ shell ("z3 -in -smt2 1>/dev/null 2>&1 <" ++ fp)
+  sltimes <- mapM solveQuery queries
+  pure $ zip queries sltimes
+  where
+    solveQuery :: FilePath -> IO Double
+    solveQuery fp = do
+      -- TODO: use createProcess and do the input redirection in Haskell.
+      (_, Just hout, _, p) <-
+        createProcess $
+          (shell ("z3 -st -in -smt2 2>/dev/null 2>&1 <" ++ fp))
+            { std_out = CreatePipe
+            }
 
-    ret <- waitForProcess p
-    if ret /= ExitSuccess
-      then error "SMT solver failed"
-      else pure ()
+      ret <- waitForProcess p
+      when (ret /= ExitSuccess) $
+        error "SMT solver failed"
+
+      -- transform the z3 output and extract the :total-time.
+      outLines <- lines <$> hGetContents hout
+      let stat = filter (not . all isLower) outLines
+      exprs <- case SMT.readSExpr $ unlines stat of
+        Just (SMT.List e, _) -> pure e
+        _ -> error "invalid statistics output"
+      timeStr <- case unsnoc exprs of
+        Just (_, SMT.Atom s) -> pure s
+        _ -> error "invalid statistics expression"
+      pure $ read timeStr
 
 main :: IO ()
 main = do
   getQueries "/tmp/queries" "/home/soeren/src/quebex/quebex-symex/test/golden/prime-numbers.qbe" [("a", QBE.Word)]
-  solveQueries "/tmp/queries"
+  results <- solveQueries "/tmp/queries"
+  forM_ results (putStrLn . showResult)
+  where
+    showResult :: (FilePath, Double) -> String
+    showResult (fname, time) = takeBaseName fname ++ "\t" ++ show time
