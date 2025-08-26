af6da106ae228f6a963333365dee2a3861dec3dc
Sören Tempel <soeren+git@soeren-tempel.net>
2025-08-26 11:43:32 +0200

    quebex-symex: Add execution benchmarks

---
+++ quebex-symex/bench/Exec.hs
@@ -0,0 +1,49 @@
+-- SPDX-FileCopyrightText: 2025 Sören Tempel <soeren+git@soeren-tempel.net>
+--
+-- SPDX-License-Identifier: MIT AND GPL-3.0-only
+
+-- TODO: Code duplication with quebex/bench/{Main.hs,Exec.hs}
+module Exec (execBench) where
+
+import Control.Monad (void)
+import Criterion.Main
+import Data.List (find)
+import Data.Word (Word64, Word8)
+import Language.QBE (globalFuncs, parse)
+import Language.QBE.Simulator (execFunc)
+import Language.QBE.Simulator.Concolic.Expression qualified as CE
+import Language.QBE.Simulator.Default.Expression qualified as DE
+import Language.QBE.Simulator.Default.State (SimState, run)
+import Language.QBE.Simulator.Expression qualified as E
+import Language.QBE.Types qualified as QBE
+
+entryFunc :: QBE.GlobalIdent
+entryFunc = QBE.GlobalIdent "entry"
+
+exec :: [CE.Concolic DE.RegVal] -> String -> IO ()
+exec params input = do
+  prog <- case parse "input" input of
+    Left err -> fail $ "parsing error: " ++ show err
+    Right pr -> pure pr
+
+  let funcs = globalFuncs prog
+  func <- case find (\f -> QBE.fName f == entryFunc) funcs of
+    Just x -> pure x
+    Nothing -> fail $ "unknown function: " ++ show entryFunc
+
+  void $ run prog (execFunc func params :: SimState (CE.Concolic DE.RegVal) (CE.Concolic Word8) (Maybe (CE.Concolic DE.RegVal)))
+
+------------------------------------------------------------------------
+
+bubbleSort :: Word64 -> Benchmarkable
+bubbleSort inputSize =
+  nfIO (readFile "bench/data/Exec/bubble-sort.qbe" >>= exec [E.fromLit QBE.Word inputSize])
+
+execBench :: Benchmark
+execBench =
+  bgroup
+    "bubble-sort"
+    [ bench "10" $ bubbleSort 25,
+      bench "50" $ bubbleSort 50,
+      bench "100" $ bubbleSort 100
+    ]
---
+++ quebex-symex/bench/SMT.hs
@@ -0,0 +1,87 @@
+-- SPDX-FileCopyrightText: 2025 Sören Tempel <soeren+git@soeren-tempel.net>
+--
+-- SPDX-License-Identifier: MIT AND GPL-3.0-only
+
+module SMT (smtBench) where
+
+import Control.Monad (when)
+import Criterion.Main
+import Data.List (find)
+import Language.QBE (globalFuncs, parse)
+import Language.QBE.Backend.Store qualified as ST
+import Language.QBE.Backend.Tracer qualified as T
+import Language.QBE.Simulator.Explorer (explore, logSolver, newEngine)
+import Language.QBE.Types qualified as QBE
+import SMTUnwind (unwind)
+import System.Exit (ExitCode (ExitSuccess))
+import System.FilePath ((</>))
+import System.IO (IOMode (WriteMode), hClose, hPutStrLn, openFile, withFile)
+import System.Process
+  ( StdStream (CreatePipe, UseHandle),
+    createProcess,
+    proc,
+    std_in,
+    std_out,
+    waitForProcess,
+  )
+
+logPath :: FilePath
+logPath = "/tmp/quebex-symex-bench.smt2"
+
+entryFunc :: QBE.GlobalIdent
+entryFunc = QBE.GlobalIdent "entry"
+
+------------------------------------------------------------------------
+
+exploreQBE :: FilePath -> [(String, QBE.BaseType)] -> IO [(ST.Assign, T.ExecTrace)]
+exploreQBE filePath params = do
+  content <- readFile filePath
+  prog <- case parse filePath content of
+    Right rt -> pure rt
+    Left err -> fail $ "Parsing error: " ++ show err
+
+  let funcs = globalFuncs prog
+  func <- case find (\f -> QBE.fName f == entryFunc) funcs of
+    Just x -> pure x
+    Nothing -> fail $ "Unable to find entry function: " ++ show entryFunc
+
+  withFile logPath WriteMode (explore' prog func)
+  where
+    explore' prog func handle = do
+      engine <- logSolver handle >>= newEngine
+      explore engine prog func params
+
+getQueries :: String -> [(String, QBE.BaseType)] -> IO String
+getQueries name params = do
+  _ <- exploreQBE ("bench" </> "data" </> "SMT" </> name) params
+  -- XXX: Uncomment this to benchmark incremental solving instead.
+  unwind logPath
+
+solveQueries :: String -> IO ()
+solveQueries queries = do
+  devNull <- openFile "/dev/null" WriteMode
+  (Just hin, _, _, p) <-
+    createProcess
+      (proc "bitwuzla" [])
+        { std_in = CreatePipe,
+          std_out = UseHandle devNull
+        }
+
+  hPutStrLn hin queries <* hClose hin
+  ret <- waitForProcess p <* hClose devNull
+  when (ret /= ExitSuccess) $
+    error "SMT solver failed"
+
+smtBench :: Benchmark
+smtBench = do
+  bgroup
+    "SMT Complexity"
+    [ benchWithEnv "prime-numbers.qbe" [("a", QBE.Word)],
+      benchWithEnv "bubble-sort.qbe" [("a", QBE.Word), ("b", QBE.Word), ("c", QBE.Word), ("d", QBE.Word)]
+    ]
+  where
+    benchSolver :: String -> String -> Benchmark
+    benchSolver name queries = bench name $ nfIO (solveQueries queries)
+
+    benchWithEnv :: String -> [(String, QBE.BaseType)] -> Benchmark
+    benchWithEnv name params = env (getQueries name params) (benchSolver name)
