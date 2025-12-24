b747e845fae391354bc58f12c20595ca1a5d01de
Sören Tempel <soeren+git@soeren-tempel.net>
2025-12-24 12:11:40 +0100

    quebex-cli: add exe:quebex for concrete QBE execution

---
+++ quebex-cli/app/quebex/Main.hs
@@ -0,0 +1,40 @@
+-- SPDX-FileCopyrightText: 2025 Sören Tempel <soeren+git@soeren-tempel.net>
+--
+-- SPDX-License-Identifier: GPL-3.0-only
+
+module Main (main) where
+
+import Control.Monad (void)
+import Data.Word (Word8)
+import Language.QBE (parseAndFind)
+import Language.QBE.CmdLine qualified as CMD
+import Language.QBE.Simulator (execFunc)
+import Language.QBE.Simulator.Default.Expression qualified as DE
+import Language.QBE.Simulator.Default.State (Env, mkEnv, run)
+import Language.QBE.Types qualified as QBE
+import Options.Applicative qualified as OPT
+
+execFile :: CMD.BasicArgs -> IO ()
+execFile opts = do
+  let filePath = CMD.optQBEFile opts
+  (prog, func) <- readFile filePath >>= parseAndFind entryFunc
+
+  env <- mkEnv prog (CMD.optMemStart opts) (CMD.optMemSize opts)
+  void $ run (env :: Env DE.RegVal Word8) (execFunc func params)
+  where
+    params :: [DE.RegVal]
+    params = []
+
+    entryFunc :: QBE.GlobalIdent
+    entryFunc = QBE.GlobalIdent "main"
+
+main :: IO ()
+main = OPT.execParser cmd >>= execFile
+  where
+    cmd :: OPT.ParserInfo CMD.BasicArgs
+    cmd =
+      OPT.info
+        (CMD.basicArgs OPT.<**> OPT.helper)
+        ( OPT.fullDesc
+            <> OPT.progDesc "Concrete execution of programs in the QBE intermediate language"
+        )
