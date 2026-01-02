a30729159918636d159eb9560359efd3f5a6ed4e
Sören Tempel <soeren+git@soeren-tempel.net>
2026-01-02 16:53:46 +0100

    quebex: expand tests for expression abstraction

--- quebex/test/Expression.hs
+++ quebex/test/Expression.hs
@@ -51,7 +51,53 @@ exprTests =
         do
           let v = E.fromLit (Q.Base Q.Word) (fromIntegral (-0x80000000 :: Int64)) :: DE.RegVal
           let r = fromJust $ v `E.shr` E.fromLit (Q.Base Q.Word) 8
-          r @?= E.fromLit (Q.Base Q.Word) 0x800000
+          r @?= E.fromLit (Q.Base Q.Word) 0x800000,
+      testCase "extend byte to word" $
+        do
+          let v = E.fromLit Q.Byte 128 :: DE.RegVal
+
+          let signExt = E.fromLit (Q.Base Q.Word) 0xffffff80 :: DE.RegVal
+          E.extend (Q.Base Q.Word) True v @?= Just signExt
+
+          let zeroExt = E.fromLit (Q.Base Q.Word) 128 :: DE.RegVal
+          E.extend (Q.Base Q.Word) False v @?= Just zeroExt,
+      testCase "extend float" $
+        do
+          let s = E.fromLit (Q.Base Q.Single) 2342 :: DE.RegVal
+          E.extend (Q.Base Q.Long) False s @?= Nothing
+
+          let d = E.fromLit (Q.Base Q.Double) 2342 :: DE.RegVal
+          E.extend (Q.Base Q.Long) False d @?= Nothing,
+      testCase "current size exceeds extend" $
+        do
+          let s = E.fromLit (Q.Base Q.Word) 2342 :: DE.RegVal
+          E.extend Q.Byte False s @?= Nothing,
+      testCase "current size equals extend" $
+        do
+          let s = E.fromLit (Q.Base Q.Word) 2342 :: DE.RegVal
+          E.extend (Q.Base Q.Word) True s @?= Nothing,
+      testCase "extract from word" $
+        do
+          let v = E.fromLit (Q.Base Q.Word) 0xdeadbeef :: DE.RegVal
+
+          let e1 = E.fromLit Q.Byte 0xef :: DE.RegVal
+          E.extract Q.Byte v @?= Just e1
+
+          let e2 = E.fromLit Q.HalfWord 0xbeef :: DE.RegVal
+          E.extract Q.HalfWord v @?= Just e2,
+      testCase "extract from float" $
+        do
+          let d = E.fromLit (Q.Base Q.Double) 2342 :: DE.RegVal
+          E.extract Q.Byte d @?= Nothing
+          E.extract (Q.Base Q.Single) d @?= Nothing
+
+          let s = E.fromLit (Q.Base Q.Single) 2342 :: DE.RegVal
+          E.extract Q.Byte s @?= Nothing
+          E.extract (Q.Base Q.Single) s @?= Nothing,
+      testCase "extract exceeds size" $
+        do
+          let v = E.fromLit (Q.Base Q.Word) 0xdeadbeef :: DE.RegVal
+          E.extract (Q.Base Q.Long) v @?= Nothing
     ]
   where
     falseValue = Just (E.fromLit (Q.Base Q.Long) 0 :: DE.RegVal)
