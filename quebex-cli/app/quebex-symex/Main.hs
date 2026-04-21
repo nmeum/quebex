0616d9908ca2031e917bfbbfe7d23dbd3699660b
Sören Tempel <soeren+git@soeren-tempel.net>
2026-04-21 14:25:42 +0200

    quebex-cli: Write .ktest files iteratively

--- quebex-cli/app/quebex-symex/Main.hs
+++ quebex-cli/app/quebex-symex/Main.hs
@@ -4,14 +4,20 @@
 
 module Main (main) where
 
+import Control.Monad.State (evalStateT, gets, liftIO)
 import Data.Binary (encodeFile)
 import Data.KTest (KTest (KTest), KTestObj, fromAssign)
 import Data.String (fromString)
-import Language.QBE.Backend.Store qualified as ST
-import Language.QBE.Backend.Tracer qualified as T
 import Language.QBE.CmdLine qualified as CMD
+import Language.QBE.Simulator (execFunc)
 import Language.QBE.Simulator.Concolic.State (mkEnv)
-import Language.QBE.Simulator.Explorer (defSolver, exploreFunc, logSolver, newEngine)
+import Language.QBE.Simulator.Explorer
+  ( Engine(expPathVars),
+    defSolver,
+    explorePath,
+    logSolver,
+    newEngine,
+  )
 import Language.QBE.Types qualified as QBE
 import Options.Applicative qualified as OPT
 import System.Directory (createDirectoryIfMissing)
@@ -57,7 +63,38 @@ optsParser =
 
 ------------------------------------------------------------------------
 
-exploreFile :: Opts -> IO [(ST.Assign, T.ExecTrace)]
+writeKTest :: FilePath -> FilePath -> Int -> [KTestObj] -> IO ()
+writeKTest directory fileArg pathID objs = do
+  createDirectoryIfMissing True directory
+  writeKTest' pathID $ mkTestCase objs
+  where
+    mkTestCase :: [KTestObj] -> KTest
+    mkTestCase = KTest [fromString fileArg]
+
+    writeKTest' :: Int -> KTest -> IO ()
+    writeKTest' n ktest = do
+      flip encodeFile ktest $
+        addExtension
+          (directory </> ("test" ++ printf "%06d" n))
+          ".ktest"
+
+exploreEntry :: Opts -> Engine -> QBE.FuncDef -> IO Int
+exploreEntry opts engine entry = evalStateT (go 1 $ execFunc entry []) engine
+  where
+    go n st = do
+      morePaths <- explorePath st
+      case optTests opts of
+        Just dir -> do
+          assign <- gets (fromAssign . expPathVars)
+          liftIO $
+            writeKTest dir (CMD.optQBEFile $ optBase opts) n assign
+        Nothing -> pure ()
+
+      if morePaths
+        then go (n + 1) st
+        else pure n
+
+exploreFile :: Opts -> IO Int
 exploreFile opts@Opts {optBase = base} = do
   (prog, func) <- CMD.parseEntryFile $ CMD.optQBEFile base
 
@@ -66,46 +103,23 @@ exploreFile opts@Opts {optBase = base} = do
     Just fn -> withFile fn WriteMode (exploreWithHandle env func)
     Nothing -> do
       engine <- newEngine env <$> defSolver
-      exploreFunc engine func params
+      exploreEntry opts engine func
   where
-    params :: [(String, QBE.ExtType)]
-    params = []
-
     exploreWithHandle env func handle = do
       engine <- newEngine env <$> logSolver handle
-      exploreFunc engine func params
+      exploreEntry opts engine func
 
-writeKTests :: FilePath -> FilePath -> [[KTestObj]] -> IO ()
-writeKTests directory fileArg objs = do
-  createDirectoryIfMissing True directory
-  mapM_ (uncurry writeKTest) $ zip [1 ..] (map mkTestCase objs)
-  where
-    mkTestCase :: [KTestObj] -> KTest
-    mkTestCase = KTest [fromString fileArg]
+------------------------------------------------------------------------
 
-    writeKTest :: Int -> KTest -> IO ()
-    writeKTest n ktest = do
-      flip encodeFile ktest $
-        addExtension
-          (directory </> ("test" ++ printf "%06d" n))
-          ".ktest"
+cmd :: OPT.ParserInfo Opts
+cmd =
+  OPT.info
+    (optsParser OPT.<**> OPT.helper)
+    ( OPT.fullDesc
+        <> OPT.progDesc "Symbolic execution of programs in the QBE intermediate language"
+    )
 
 main :: IO ()
 main = do
-  args <- OPT.execParser cmd
-  paths <- exploreFile args
-
-  putStrLn $ "\n---\nAmount of paths: " ++ show (length paths)
-  case optTests args of
-    Just dir ->
-      writeKTests dir (CMD.optQBEFile $ optBase args) $
-        map (fromAssign . fst) paths
-    Nothing -> pure ()
-  where
-    cmd :: OPT.ParserInfo Opts
-    cmd =
-      OPT.info
-        (optsParser OPT.<**> OPT.helper)
-        ( OPT.fullDesc
-            <> OPT.progDesc "Symbolic execution of programs in the QBE intermediate language"
-        )
+  numPaths <- OPT.execParser cmd >>= exploreFile
+  putStrLn $ "\n---\nAmount of paths: " ++ show numPaths
