73a6029a9d2ee07eaf55d5f05977545111ad0504
Sören Tempel <tempel@ibr.cs.tu-bs.de>
2026-06-11 12:31:57 +0200

    quebex: Expand documentation

--- quebex/src/Language/QBE/Simulator/Expression.hs
+++ quebex/src/Language/QBE/Simulator/Expression.hs
@@ -2,6 +2,11 @@
 --
 -- SPDX-License-Identifier: GPL-3.0-only
 
+-- | This module provides a generic expression language used to describe
+-- arithmetic and logic operations on instruction operands in the abstract
+-- 'Language.QBE.Simulator' description of QBE semantics. Therefore, in
+-- addition to the 'Language.QBE.Simulator.State.Simulator' monad, it is the
+-- central component for the abstract description of QBE's semantics.
 module Language.QBE.Simulator.Expression where
 
 import Data.Char qualified as C
@@ -10,10 +15,16 @@ import Language.QBE.Types qualified as QBE
 
 -- | Generic expression abstraction operating on values of type 'QBE.ExtType'.
 -- Values are either fixed-size bitvectors (8-, 16, 32-, or 64-bit) or
--- single-precision or double-precision floating point values.
+-- single-precision or double-precision floating point values. The value type
+-- must be tracked internally by the 'ValueRepr' instance. Operations on the
+-- value must return 'Nothing' if the operation is performed on values of
+-- different types.
 class ValueRepr v where
+  -- | Create a 'ValueRepr' from an integer literal.
+  --
   -- TODO: rename fromLit to fromInt
   fromLit :: QBE.ExtType -> Word64 -> v
+
   fromFloat :: Float -> v
   fromDouble :: Double -> v
   toWord64 :: v -> Word64
@@ -36,51 +47,104 @@ class ValueRepr v where
   -- of 'ExtType' exceeds the size of 'v'.
   extract :: QBE.ExtType -> v -> Maybe v
 
+  -- | Addition.
   add :: v -> v -> Maybe v
+
+  -- | Subtraction.
   sub :: v -> v -> Maybe v
+
+  -- | Multiplication.
   mul :: v -> v -> Maybe v
+
+  -- | Unsigned division.
   div :: v -> v -> Maybe v
+
+  -- | Unsigned remainder.
   urem :: v -> v -> Maybe v
+
+  -- | Signed remainder.
   srem :: v -> v -> Maybe v
+
+  -- | Unsigned division.
   udiv :: v -> v -> Maybe v
+
+  -- | Bitwise or.
   or :: v -> v -> Maybe v
+
+  -- | Bitwise xor.
   xor :: v -> v -> Maybe v
+
+  -- | Bitwise and.
   and :: v -> v -> Maybe v
 
+  -- | Unary negation.
   neg :: v -> Maybe v
 
+  -- | Arithmetic right shift, preserving the sign bit of the shifted value.
+  -- Shift amount must always be a 32-bit value, the shifted value must be 32- or 64-bit.
   sar :: v -> v -> Maybe v
+
+  -- | Logical shift right, filling the newly freed bits with zeroes.
+  -- Shift amount must always be a 32-bit value, the shifted value must be 32- or 64-bit.
   shr :: v -> v -> Maybe v
+
+  -- | Logical shift left, always fills the freed bits with zeroes.
+  -- Shift amount must always be a 32-bit value, the shifted value must be 32- or 64-bit.
   shl :: v -> v -> Maybe v
 
+  -- | Check for equality.
   eq :: v -> v -> Maybe v
+
+  -- | Check if two values are not equal.
   ne :: v -> v -> Maybe v
+
+  -- | Signed less than or equal to.
   sle :: v -> v -> Maybe v
+
+  -- | Signed less than.
   slt :: v -> v -> Maybe v
+
+  -- | Signed greater than or equal to.
   sge :: v -> v -> Maybe v
+
+  -- | Signed greater than.
   sgt :: v -> v -> Maybe v
+
+  -- | Unsigned less than or equal to.
   ule :: v -> v -> Maybe v
+
+  -- | Unsigned less than.
   ult :: v -> v -> Maybe v
+
+  -- | Unsigned greater than or equal to.
   uge :: v -> v -> Maybe v
+
+  -- | Unsigned greater then.
   ugt :: v -> v -> Maybe v
 
-  -- Ordered: no operand is a NaN.
+  -- | Ordered, no operand is a NaN.
+  -- Only defined for floating points, must return 'Nothing' otherwise.
   ord :: v -> v -> Maybe v
 
-  -- Unordered: At least one operand is a NaN
+  -- | Unordered, at least one operand is a NaN.
+  -- Only defined for floating points, must return 'Nothing' otherwise.
   unord :: v -> v -> Maybe v
   unord lhs rhs = ord lhs rhs >>= neg
 
+-- | Convert a string to a list of 8-bit values represented through 'ValueRepr'.
 fromString :: (ValueRepr v) => String -> [v]
 fromString = map (\c -> fromLit QBE.Byte (fromIntegral $ C.ord c))
 
+-- | Inverse of 'fromString'.
 toString :: (ValueRepr v) => [v] -> String
 toString = map (\b -> C.chr (fromIntegral $ toWord64 b))
 
+-- | Convert a Boolean value to a 64-bit value in 'ValueRepr'.
 boolToValue :: (ValueRepr v) => Bool -> v
 boolToValue True = fromLit (QBE.Base QBE.Long) 1
 boolToValue False = fromLit (QBE.Base QBE.Long) 0
 
+-- | Map a 'QBE.IntCmpOp' to the corresponding function from 'ValueRepr'.
 compareIntExpr :: (ValueRepr v) => QBE.IntCmpOp -> (v -> v -> Maybe v)
 compareIntExpr QBE.IEq = eq
 compareIntExpr QBE.INe = ne
@@ -94,6 +158,7 @@ compareIntExpr QBE.IUge = uge
 compareIntExpr QBE.IUgt = ugt
 {-# INLINE compareIntExpr #-}
 
+-- | Map a 'QBE.FloatCmpOp' to the corresponding function from 'ValueRepr'.
 compareFloatExpr :: (ValueRepr v) => QBE.FloatCmpOp -> (v -> v -> Maybe v)
 compareFloatExpr QBE.FEq = eq
 compareFloatExpr QBE.FNe = ne
--- quebex/src/Language/QBE/Simulator/State.hs
+++ quebex/src/Language/QBE/Simulator/State.hs
@@ -3,6 +3,9 @@
 -- SPDX-License-Identifier: GPL-3.0-only
 {-# LANGUAGE FunctionalDependencies #-}
 
+-- | This module defines the abstract 'Simulator' monad and thus provides the primitives
+-- used by "Language.QBE.Simulator" to describe the semantics of the QBE intermediate
+-- representation.
 module Language.QBE.Simulator.State
   ( StackFrame (..),
     newStackFrame,
@@ -69,28 +72,56 @@ data SomeFunc m v
   = SSimFunc ([v] -> m (Maybe v))
   | SFuncDef QBE.FuncDef
 
--- This is an “abstract monad” representing the Simulator and allowing
--- interaction with an encapsulated Simulator state 'm'. The module
--- 'Language.QBE.Simulator.Default.State' provides an implementation of
--- a polymorphic Simulator state implement over a State monad.
+-- | This is an “abstract monad” representing the Simulator and allowing
+-- interaction with an encapsulated Simulator state 'm'. Conceptually,
+-- this monads describes the primitives based on which the semantics of
+-- the QBE intermediate representation are abstractly described in
+-- 'Language.QBE.Simulator'.
 --
--- The idea is inspired by Bourgeat et al. https://doi.org/10.1145/3607833
+-- An instance of this monad then provides concrete semantics for these
+-- primitives. For example, the module "Language.QBE.Simulator.Default.State"
+-- provides an implementation of a polymorphic Simulator state implement over a
+-- "Control.Monad.State" monad.
+--
+-- The idea is inspired by Bourgeat et al. <https://doi.org/10.1145/3607833>.
 class (E.ValueRepr v, MonadError EvalError m) => Simulator m v | m -> v where
+  -- | Check if a value of type 'ValueRepr' evaluates to true. This is used
+  -- within "Language.QBE.Simulator" to implement conditional jumps.
   isTrue :: v -> m Bool
+
+  -- | Convert a value of type 'ValueRepr' to a 'MEM.Address' that can be
+  -- used to index a "Language.QBE.Simulator.Memory".
   toAddress :: v -> m MEM.Address
 
+  -- | Lookup the address of a data symbol.
   lookupSymbol :: QBE.GlobalIdent -> m (Maybe MEM.Address)
 
+  -- | Find a function by name, required to implement [call instructions](https://c9x.me/compile/doc/il-v1.2.html#Call).
   findFunc :: QBE.GlobalIdent -> m (Maybe (SomeFunc m v))
+
+  -- | Find a function by "text segment" address, used for the implementation of function pointers.
   findFuncByAddr :: MEM.Address -> m (Maybe (SomeFunc m v))
 
+  -- | Return the 'StackFrame' of the currently executed function.
   activeFrame :: m (StackFrame v)
+
+  -- | Push a new 'StackFrame' onto the function call stack.
   pushStackFrame :: StackFrame v -> m ()
+
+  -- | Pop the current stack frame from the function call stack.
+  -- Should throw 'EmptyStack' when invoked on an empty function call stack.
   popStackFrame :: m (StackFrame v)
+
+  -- | Get the current value of the stack pointer.
   getSP :: m v
+
+  -- | Set the value of the stack pointer.
   setSP :: v -> m ()
 
+  -- | Write a value to memory.
   writeMemory :: MEM.Address -> QBE.ExtType -> v -> m () -- TODO: LoadType?
+
+  -- | Read a value from memory.
   readMemory :: QBE.LoadType -> MEM.Address -> m v
 
 liftMaybe :: (MonadError EvalError m) => EvalError -> Maybe a -> m a
@@ -122,6 +153,8 @@ runBinary ty op lhs rhs =
   liftMaybe TypingError (op lhs rhs) >>= subType ty
 {-# INLINEABLE runBinary #-}
 
+-- | Modify the current 'StackFrame', e.g. to add a new local variable to it.
+-- If the function call stack is currently empty an 'EmptyStack' error is thrown.
 modifyFrame :: (Simulator m v) => (StackFrame v -> StackFrame v) -> m ()
 modifyFrame func = do
   frame <- popStackFrame
@@ -130,12 +163,14 @@ modifyFrame func = do
 
 -- | Align a stack address. Contrary to 'MEM.alignAddr', this rounds down to
 -- the nearest aligned addressed (not up) as the stack grows downward. Further,
--- since the SP representation is presently not fixed, it operates on 'ValueRepr'.
+-- since the SP representation is presently not fixed, it operates on 'E.ValueRepr'.
 stackAlign :: (E.ValueRepr v) => v -> v -> Maybe v
 stackAlign addr alignment =
   addr `E.urem` alignment >>= (addr `E.sub`)
 {-# INLINEABLE stackAlign #-}
 
+-- | Allocate a given amount of bytes on the stack with the given alignment.
+-- Advances the stack pointer accordingly.
 stackAlloc :: (Simulator m v) => v -> Word64 -> m v
 stackAlloc size align = do
   stkPtr <- getSP
